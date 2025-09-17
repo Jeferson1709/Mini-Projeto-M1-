a883ca8da91b25a433460bb4150ae2337e3b4d3c
Jeferson Alves <jefersonalvessantos62@gmail.com>
2025-09-17 11:19:09 -0300

    feat: adicionar função de validação de dados do estudante

---
+++ gerenciador-de-estudantes/utils.js
@@ -0,0 +1,20 @@
+
+//Função para válidade as informações.
+export function validarDadosDoEstudante(nome, idade, notas) {
+  if (notas.some(nota => typeof nota !== "number" || isNaN(nota))) {
+    return "❌ Todas as notas devem ser números."
+  }
+  if (Number(nome) || typeof nome !== "string") {
+    return "❌ Nome inválido!"
+  }
+  if (!Number.isInteger(idade) || idade <= 0) {
+    return "❌ Idade inválida!";
+  }
+  if (notas.length !== 3 ) {
+    return "❌ É necessário informar exatamente três notas.";
+  }
+  if (!Array.isArray(notas) || notas.some(n => n < 0 || n > 10)) {
+    return "❌ Notas inválidas! Devem estar entre 0 e 10.";
+  }
+  
+}
